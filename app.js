cdbc4073a27e96fc25509f4fc6aeb675318ce44f
jenn083 <nguyenj1203@gmail.com>
2016-12-13 21:07:43 -0800

    added click event on pics

--- app.js
+++ app.js
@@ -1,6 +1,93 @@
 
+/////////////////////////////////////////////////////////
+// console.log('-----------');
+// console.log(oldArray, 'old array');
+// console.log(newArray, 'new array');
+
+// for (var i=0; i<100; i++) {
+//   makeArrayOfThreeNumbers();
+//   console.log('----New Run of making an array-------');
+//   console.log(oldArray, 'old array');
+//   console.log(newArray, 'new array');
+//
+// }//test code for dupes
+////////////////////////////////////////////////
+// function showThreePics() {
+//   // this will place three new images on the page
+//   makeArrayOfThreeNumbers();
+//   left.src = allProducts[newArray[0]].filePath;
+//   allProducts[newArray[0]].views+=1;
+//   center.src = allProducts[newArray[1]].filePath;
+//   allProducts[newArray[1]].views+=1;
+//   right.src = allProducts[newArray[2]].filePath;
+//   allProducts[newArray[2]].views+=1;
+//
+// }
+
+// function renderList() {
+  // display a list of items and total clicks/views
+// }
+
+// function handleClick(event) {
+//   event.preventDefault();
+//   // identify who was clicked
+//   console.log(event.target, 'was clicked');//tracking which image was clicked
+//   // alert for clicks not on images
+//   if(event.target.id === 'pic-container') {
+//     alert('Click on a PIC!');
+//   }
+//   // tally the click
+//   if(event.target.id==='left') {
+//     allProducts[newArray[0]].click+=1;
+//     console.log(allProducts[newArray[0]]);
+//   }
+
+  // if(event.target.id==='center') {
+  //   allProducts[newArray[1]].click+=1;
+  //   console.log(allProducts[newArray[1]]);
+  // }
+
+  // if(event.target.id==='right') {
+  //   allProducts[newArray[2]].click+=1;
+  //   console.log(allProducts[newArray[2]]);
+  // }
+  // console.table(allProducts);
+  // // prevent duplicates// checked
+  // // check whether total clicks <25
+  // clickCount+=1;
+  // console.log(clickCount, 'total clicks so far');
+  // if (clickCount >25) {
+  //   return alert('you are out of clicks');
+  // }
+  //after 25, remove event listeners on picNames
+
+    //after 25, show "Results" button
+    // clear old images
+// display 3 new images
+
+//   showThreePics();
+// }
+// ++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++
+// CODE THAT RUNS ON PAGE LOAD
+// (EXECUTE ACTIONS)
+// ++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++
+
+// showThreePics();
+// picContainer.addEventListener('click', handleClick);
+
+
 'use strict'
 var allProducts = [];
+// var clickCounter = 0;
+var leftImg = document.getElementById('left');
+var rightImg = document.getElementById('right');
+var centerImg = document.getElementById('center');
+var container_box = document.getElementById('img_container');
+// var clicks = 0;
+// var totalViews = 0;
+// var img_container = document.getElementById('img_container');
 console.log(allProducts);
 
 function ImageMaker(filePath,fileName) {
@@ -12,56 +99,107 @@ function ImageMaker(filePath,fileName) {
 }//end of imageMaker function
 //////////////////////////////////////////////////////////////////////////
 // function allImages() {
-new ImageMaker('assets/bag.jpg','bag');
-new ImageMaker('assets/banana.jpg','banana');
-new ImageMaker('assets/bathroom.jpg','bathroom');
-new ImageMaker('assets/boots.jpg','boots');
-new ImageMaker('assets/breakfast.jpg','breakfast');
-new ImageMaker('assets/bubblegum.jpg','bubblegum');
-new ImageMaker('assets/chair.jpg','chair');
-new ImageMaker('assets/cthulhu.jpg','cthulhu');
-new ImageMaker('assets/dog-duck.jpg','dogduck');
-new ImageMaker('assets/dragon.jpg','dragon');
-new ImageMaker('assets/pen.jpg','pen');
-new ImageMaker('assets/pet-sweep.jpg','petsweep');
-new ImageMaker('assets/scissors.jpg','scissors');
-new ImageMaker('assets/shark.jpg','shark');
-new ImageMaker('assets/sweep.png','sweep');
-new ImageMaker('assets/tauntaun.jpg','tauntaun');
-new ImageMaker('assets/unicorn.jpg','unicorn');
-new ImageMaker('assets/usb.gif','usb');
-new ImageMaker('assets/water-can.jpg','watercan');
-new ImageMaker('assets/wine-glass.jpg','wine');
+new ImageMaker('img/bag.jpg','bag');
+new ImageMaker('img/banana.jpg','banana');
+new ImageMaker('img/bathroom.jpg','bathroom');
+new ImageMaker('img/boots.jpg','boots');
+new ImageMaker('img/banana.jpg','banana');
+new ImageMaker('img/bubblegum.jpg','bubblegum');
+new ImageMaker('img/chair.jpg','chair');
+new ImageMaker('img/cthulhu.jpg','cthulhu');
+new ImageMaker('img/dog_duck.jpg','dogduck');
+new ImageMaker('img/dragon.jpg','dragon');
+new ImageMaker('img/pen.jpg','pen');
+new ImageMaker('img/pet_sweep.jpg','petsweep');
+new ImageMaker('img/scissors.jpg','scissors');
+new ImageMaker('img/shark.jpg','shark');
+new ImageMaker('img/sweep.jpg','sweep');
+new ImageMaker('img/tauntaun.jpg','tauntaun');
+new ImageMaker('img/unicorn.jpg','unicorn');
+new ImageMaker('img/usb.jpg','usb');
+new ImageMaker('img/water_can.jpg','watercan');
+new ImageMaker('img/wine_glass.jpg','wine');
 // }
 
 // allImages();
 
+/////////////////////////////////////////////////////////////
 
-
-function selectRandomLeft() {
-  var randomIndex= Math.floor(Math.random() * allProducts.length);
-  console.log(allProducts[randomIndex]);
+function selectRandomPic() {
   var left = document.getElementById('left');
-  // left.src = allProducts[randomIndex[0]].src;
-  left.src=allProducts[randomIndex].filePath;
-}
-selectRandomLeft();
+  var randomIndexLeft = Math.floor(Math.random() * allProducts.length);
+  left.src=allProducts[randomIndexLeft].filePath;
+  var leftItem = allProducts[randomIndexLeft];
+  allProducts.splice(randomIndexLeft, 1);
 
-function selectRandomCenter() {
-  var randomIndex= Math.floor(Math.random() * allProducts.length);
-  console.log(allProducts[randomIndex]);
   var center = document.getElementById('center');
-  // center.src = allProducts[randomIndex[1]].src;
-  center.src=allProducts[randomIndex].filePath;
-}
-selectRandomCenter();
+  var randomIndexCenter = Math.floor(Math.random() * allProducts.length);
+  center.src=allProducts[randomIndexCenter].filePath;
+  var centerItem = allProducts[randomIndexCenter];
 
-function selectRandomRight() {
-  var randomIndex= Math.floor(Math.random() * allProducts.length);
-  console.log(allProducts[randomIndex]);
   var right = document.getElementById('right');
-  // right.src = allProducts[randomIndex[2]].src;
-  right.src=allProducts[randomIndex].filePath;
+  allProducts.splice(randomIndexCenter,1);
+  var randomIndexRight = Math.floor(Math.random() * allProducts.length);
+  right.src=allProducts[randomIndexRight].filePath;
+  var rightItem = allProducts[randomIndexRight];
+  allProducts.push(leftItem,centerItem);
+}
+selectRandomPic();
+
+// var selectImg = section.getElementsByTagName('img');
+// selectImg.addEventListener('click', selectRandomPic);
+
+
+leftImg.addEventListener('click', selectRandomPic);
+
+rightImg.addEventListener('click', selectRandomPic);
+
+centerImg.addEventListener('click', selectRandomPic);
+
+
+
+function clickHandler(event) {
+  event.preventDefault();
+  if(event.target.id === 'img_container'){
+    return alert('Please click on the image. Not the surrounding area.')
+  }
 }
 
-selectRandomRight();
+
+
+
+
+
+
+
+container_box.addEventListener('click',clickHandler);
+
+// var newArray
+////////////////////////////////////////////////////////////////////
+
+// while(leftRandomIndex===centerRandomIndex||leftRandomIndex===rightRandomIndex||centerRandomIndex===rightRandomIndex){
+//   var leftRandomIndex = Math.floor(Math.random()*allProducts.length);
+//   var centerRandomIndex = Math.floor(Math.random()*allProducts.length);
+//   var rightRandomIndex = Math.floor(Math.random()*allProducts.length);
+// }
+
+
+// function clickHandler(event) {
+//   event.preventDefault();
+//
+//     if(event.target.src==='img_container'){
+//       alert('Please click on an image');
+//     }else{
+//       console.log(event.target.id);
+//       clicks += 1;
+//
+//    }
+//     if (event.target.src===allProducts[leftRandomIndex]){
+//
+//       if(count===25) {
+//         return false;
+//       }
+//     }
+//   }
+//
+// img_container.addEventListener('click',clickHandler);
--- user_stories.md
+++ user_stories.md
@@ -6,9 +6,11 @@ As a developer I want clean and concise code so that other developers can read m
 As a developer I want to logically plan out my code so that I can understand and implement the code.
 As a developer I want to ACP at each step/feature so that I don't lose my work.
 As a developer I want to keep my code dry so that decrease page load.
+As a developer I want to make sure to show the reset button after 25 clicks.
+As a developer I want to make sure to keep track of the views.
 
 
-User:
+Focus Group:
 As a user I want the site to be organize and have a logical flow so that I can easily participate and interact with the site.
 As a user I want to be able to easily access the information that I need quickly so that I don't waste too much time digging through irrelevant content.
 As a user I want the site to provide some kind of instructions so I can follow along.  
@@ -17,8 +19,8 @@ As a user I want the site to provide helpful hints when I've done something wron
 
 
 
-Shareholder:
-As a shareholder I want the page to look professional and appealing to our target customers.
-As a shareholder I want the page to include our company's logo and our mission so that users can understand who we are and what we stand for.
-As a shareholder I want the page to have practical uses so that users will continue to visit the site again and again.
-As a shareholder I want the page to work correctly so that we don't lose money.
+Marketing Research Team:
+As a MRT I want the page to look professional and appealing to our target customers.
+As a MRT I want the page to include our company's logo and our mission so that users can understand who we are and what we stand for.
+As a MRT I want the page to be user friendly so that users to retain cooperation.
+As a MRT I want the page to work correctly so that we don't lose money.
